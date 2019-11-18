cbf310141101280392639e647488c7796dfe89e4
Robbie Gay <robbiecgay@gmail.com>
2019-11-18 14:11:52 -0500

    Got sound to work, tried to work on visuals

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Tone from 'tone';
+import { CanvasSpace, Pt, Group } from "pts";
+import $ from "jquery";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" id='hello'>
+        {/* var synth = new Tone.Synth().toMaster();
+        synth.triggerAttackRelease("C4", "8n"); */}
+        <input type="text" value="75" className="dial" />
+
+        <script>
+          $(function() {
+            $(".dial").knob();
+          });
+        </script> 
       </header>
     </div>
   );
 }
+
+
+
+
+// var space = new CanvasSpace("#hello");
+// space.setup({ bgcolor: "#fff" });
+// SOUND
+// var reverb = new Tone.Reverb(10);
+// var synth = new Tone.DuoSynth().connect('reverb');
+// synth.triggerAttackRelease("B2", "1n").toMaster();
+// reverb.connect(synth.triggerAttackRelease("B2", "1n"));
+// synth.toMaster();
+
+// Nice sounding: DuoSynth, MonoSynth, NoiseSynth, PolySynth
+
+// var freeverb = new Tone.Freeverb().toMaster();
+// freeverb.dampening.value = 1000;
+// //routing synth through the reverb
+// var synth1 = new Tone.AMSynth().connect(freeverb);
+// synth1.triggerAttackRelease("B2", "1n");
+
+// var reverb = new Tone.Freeverb(1).toMaster(); 
+// var crush = new Tone.BitCrusher(4).connect(reverb);
+// var synth = new Tone.DuoSynth().connect(crush);
+// synth.triggerAttackRelease("B2", "1n");
+
 
 export default App;
